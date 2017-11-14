bbbb272bf05c6e08ba046306fed33f61cb218c06
JHades <jhades.dev@gmail.com>
2017-11-14 12:12:58 +0100

    Angular Material Course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon'
+
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        TopMenuComponent
     ],
     imports: [
         BrowserModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        MatMenuModule,
+        MatIconModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ src/app/top-menu/top-menu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'top-menu',
+  templateUrl: './top-menu.component.html',
+  styleUrls: ['./top-menu.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TopMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
