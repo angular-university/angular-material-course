4e8c3435d724fa3f35e4d85f008605494e5baac2
Your Name <you@example.com>
2017-11-16 13:08:51 +0100

    Angular Material Course

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
