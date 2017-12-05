362695909e016b8d85a9b14bf3202b3b83c12a38
Your Name <you@example.com>
2017-12-05 15:17:46 +0100

    Angular Material Course

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -26,6 +26,8 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
     @ViewChild(MatSort) sort: MatSort;
 
+    @ViewChild('input') input: ElementRef;
+
     constructor(private route: ActivatedRoute,
                 private coursesService: CoursesService) {
 
@@ -43,18 +45,34 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
 
+        fromEvent(this.input.nativeElement,'keyup')
+            .pipe(
+                debounceTime(150),
+                distinctUntilChanged(),
+                tap(() => {
+                    this.paginator.pageIndex = 0;
+
+                    this.loadLessonsPage();
+                })
+            )
+            .subscribe();
+
         merge(this.sort.sortChange, this.paginator.page)
         .pipe(
             startWith(null),
-            tap(() => this.dataSource.loadLessons(
-                this.course.id, '',
-                this.sort.direction,
-                this.paginator.pageIndex,
-                this.paginator.pageSize))
+            tap(() => this.loadLessonsPage())
         )
         .subscribe();
 
+    }
 
+    loadLessonsPage() {
+        this.dataSource.loadLessons(
+            this.course.id,
+            this.input.nativeElement.value,
+            this.sort.direction,
+            this.paginator.pageIndex,
+            this.paginator.pageSize);
     }
 
 
