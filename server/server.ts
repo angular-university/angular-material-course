7f179853026052bcb9411188d0fe3b07af4ad8cd
Aleš Orel <ales.orel@comland.si>
2020-12-03 23:23:58 +0100

    Server addres port fix.

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,9 @@
 
 
 import * as express from 'express';
-import {Application} from "express";
-import {getAllCourses, getCourseById} from "./get-courses.route";
-import {searchLessons} from "./search-lessons.route";
+import {Application} from 'express';
+import {getAllCourses, getCourseById} from './get-courses.route';
+import {searchLessons} from './search-lessons.route';
 
 
 const app: Application = express();
@@ -18,8 +18,8 @@ app.route('/api/lessons').get(searchLessons);
 
 
 
-const httpServer:any = app.listen(9000, () => {
-    console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);
+const httpServer = app.listen(9000, () => {
+    console.log('HTTP REST API Server running at http://localhost:' + httpServer.address()['port']);
 });
 
 
