b0121726f969ad883d40063de306b21b9f1bb0cd
Your Name <you@example.com>
2021-10-22 15:14:53 +0200

    Angular Material In Depth

--- server/search-lessons.route.ts
+++ server/search-lessons.route.ts
@@ -12,15 +12,28 @@ export function searchLessons(req: Request, res: Response) {
     const queryParams = req.query;
 
     const courseId = queryParams.courseId,
-          filter = queryParams.filter || '',
-          sortOrder = queryParams.sortOrder,
-          pageNumber = parseInt(queryParams.pageNumber) || 0,
-          pageSize = parseInt(queryParams.pageSize);
+        filter = queryParams.filter || '',
+        sortOrder = queryParams.sortOrder,
+        pageNumber = parseInt(queryParams.pageNumber) || 0,
+        pageSize = parseInt(queryParams.pageSize),
+        sortColumn = queryParams.sortColumn ?? "seqNo";
 
-    let lessons = Object.values(LESSONS).filter(lesson => lesson.courseId == courseId).sort((l1, l2) => l1.id - l2.id);
+    let lessons = Object.values(LESSONS)
+        .filter(lesson => lesson.courseId == courseId)
+        .sort((l1, l2) => {
+            if (l1[sortColumn] > l2[sortColumn]) {
+                return 1;
+            }
+            else if (l1[sortColumn] < l2[sortColumn]) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        });
 
     if (filter) {
-       lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
+        lessons = lessons.filter(lesson => lesson.description.trim().toLowerCase().search(filter.toLowerCase()) >= 0);
     }
 
     if (sortOrder == "desc") {
