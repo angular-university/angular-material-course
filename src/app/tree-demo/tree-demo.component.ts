3c512918051f45777b0ffe984a44fbb039b73f28
JHades <jhades.dev@gmail.com>
2022-01-06 15:09:32 +0100

    Angular Material In Depth - preparation for the material tree section

---
+++ src/app/tree-demo/tree-demo.component.ts
@@ -0,0 +1,69 @@
+import {Component, OnInit} from '@angular/core';
+import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource} from '@angular/material/tree';
+import {FlatTreeControl, NestedTreeControl} from '@angular/cdk/tree';
+
+interface CourseNode {
+  name: string;
+  children?: CourseNode[];
+}
+
+const TREE_DATA: CourseNode[] = [
+  {
+    name: 'Angular For Beginners',
+    children: [
+      {
+        name: 'Introduction to Angular'
+      },
+      {
+        name: 'Angular Component @Input()'
+      },
+      {
+        name: 'Angular Component @Output()'
+      }
+    ],
+  },
+  {
+    name: 'Angular Material In Depth',
+    children: [
+      {
+        name: 'Introduction to Angular Material',
+        children: [
+          {
+            name: 'Form Components'
+          },
+          {
+            name: 'Navigation and Containers'
+          }
+        ],
+      },
+      {
+        name: 'Advanced Angular Material',
+        children: [
+          {
+            name: 'Custom Themes'
+          },
+          {
+            name: 'Tree Components'
+          }
+        ],
+      },
+    ],
+  },
+];
+
+@Component({
+  selector: 'tree-demo',
+  templateUrl: 'tree-demo.component.html',
+  styleUrls: ['tree-demo.component.scss']
+})
+export class TreeDemoComponent implements OnInit {
+
+
+  ngOnInit() {
+
+
+  }
+
+}
+
+
