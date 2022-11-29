545694582334835bd87af30d85d3951926b8edaf
jhades <jhades.dev@gmail.com>
2022-11-29 17:28:45 +0100

    Upgraded to Angular 15 and Node 18

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -49,7 +49,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
