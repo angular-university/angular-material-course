ad3f1bc13227df3e5396d676245ab22d7fc6be3d
jhades <jhades.dev@gmail.com>
2023-05-09 11:46:56 +0200

    Upgraded to Angular 16

--- src/app/services/course.resolver.ts
+++ src/app/services/course.resolver.ts
@@ -1,24 +1,14 @@
-
-
-
-import {Injectable} from "@angular/core";
-import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {inject} from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import {Course} from "../model/course";
 import {Observable} from "rxjs";
 import {CoursesService} from "./courses.service";
 
 
+export function courseResolver(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
 
-@Injectable()
-export class CourseResolver implements Resolve<Course> {
+  const coursesService = inject(CoursesService);
 
-    constructor(private coursesService:CoursesService) {
-
-    }
-
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Course> {
-        return this.coursesService.findCourseById(route.params['id']);
-    }
+  return coursesService.findCourseById(route.params['id']);
 
 }
-
